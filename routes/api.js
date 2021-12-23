f874e99ea0c513b783171f7d8700c8834c3538a7
jonjonnyjonjon <70429099+jonjonnyjonjon@users.noreply.github.com>
2021-12-23 18:34:14 +0800

    refactor: added string formatting and new query field
    User's string input has to be formatted to match results from database. A new "term" field is added to the query to accommodate the various bids in various semesters.

--- routes/api.js
+++ routes/api.js
@@ -3,12 +3,17 @@ const router = express.Router();
 const Bidding = require('../models/bidding');
 
 router.get("/bidding", async (req, res) => {
+    const userInput = req.query.userInput;
+    let [term, courseCode, biddingWindow, instructor] = userInput.split(", ");
+    let bwSplit = biddingWindow.split("R").join("").split("W");
+    let bwFormatted = `Round ${bwSplit[0]} Window ${bwSplit[1]}`;
     try {
         const bidding = await Bidding.find(
             {   
-                courseCode: req.query.courseCode,
-                biddingWindow: req.query.biddingWindow,
-                instructor: req.query.instructor
+                term: term,
+                courseCode: courseCode.toUpperCase(),
+                biddingWindow: bwFormatted,
+                instructor: instructor.toUpperCase()
             }
         );
         res.send(bidding);
