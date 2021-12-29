45e5dd42c41f4383b3a444a550d65a5ea23bdaf3
nicwongg <72185415+nicwongg@users.noreply.github.com>
2021-12-29 19:21:49 +0800

    feat: Create GET endpoint for retrieving list of instructors
    Created a new endpoint (/instructors) for retrieving a list of instructors for Option 2 of the telebot.
    
    Also refactored the /bidding endpoint to remove the formatting of user input in the API endpoint itself. This is currently being done on the telebot 'frontend' codebase.

--- routes/api.js
+++ routes/api.js
@@ -3,22 +3,39 @@ const router = express.Router();
 const Bidding = require('../models/bidding');
 
 router.get("/bidding", async (req, res) => {
-    const userInput = req.query.userInput;
-    let [term, courseCode, biddingWindow, instructor] = userInput.split(", ");
-    let bwSplit = biddingWindow.split("R").join("").split("W");
-    let bwFormatted = `Round ${bwSplit[0]} Window ${bwSplit[1]}`;
     try {
         const bidding = await Bidding.find(
-            {   
-                term: term,
-                courseCode: courseCode.toUpperCase(),
-                biddingWindow: bwFormatted,
-                instructor: instructor.toUpperCase()
+            {
+                term: req.query.term,
+                courseCode: req.query.courseCode.toUpperCase(),
+                biddingWindow: req.query.biddingWindow,
+                instructor: req.query.instructor.toUpperCase()
             }
         );
         res.send(bidding);
     } catch (error) {
         res.send(error)
+    }
+});
+
+router.get("/instructors", async (req, res) => {
+    try {
+        const bidding = await Bidding.find(
+            {
+                term: req.query.term,
+                courseCode: req.query.courseCode.toUpperCase(),
+                biddingWindow: req.query.biddingWindow
+            }
+        ).distinct('instructor');
+
+        if (bidding.length > 0) {
+            res.status(200).send(bidding);
+        } else if (bidding.length == 0) {
+            res.status(204).send();
+        }
+
+    } catch (error) {
+        res.send(error);
     }
 });
 
